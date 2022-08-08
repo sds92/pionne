d68ddc458c8cac920e80c7a5d839f0d4bf469632
Сафронов Дмитрий Сергеевич <safronov_ds@mgtniip.ru>
2022-08-08 06:16:07 +0300

    stable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,19 +7,22 @@ import React from 'react';
 function MyApp({ Component, pageProps }: AppProps) {
   const { getCart, cart } = useCart();
 
-  
+  const handleCartSync = React.useCallback(() => {
+    getCart();
+  }, [getCart]);
 
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
-      window.addEventListener('storage', getCart);
+      window.addEventListener('storage', handleCartSync);
     }
 
-    getCart();
+    if (cart.length === 0) getCart();
 
     return () => {
-      window.removeEventListener('storage', getCart);
+      window.removeEventListener('storage', handleCartSync);
     };
-  }, [getCart]);
+  }, [cart, getCart, handleCartSync]);
+  
   return (
     <Layout>
       <Component {...pageProps} />
