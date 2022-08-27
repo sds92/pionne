4c69e16948ae2e630db0b3c82a441c9468e52eea
Сафронов Дмитрий Сергеевич <safronov_ds@mgtniip.ru>
2022-08-27 12:59:07 +0300

    stable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.{ts,tsx}/,
+  //     use:
+  //   })
+  //   return config;
+  // },
+  nextConfig,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    // loader: 'custom',
+  },
+};
